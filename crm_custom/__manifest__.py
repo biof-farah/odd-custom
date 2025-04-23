a365e663882c5d36b2f656e48a195ea04ec190b2
farah sharban <100386507+biof-farah@users.noreply.github.com>
2025-04-23 13:31:50 +0200

    Add files via upload

---
+++ crm_custom/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'CRM custom',
+    'description': 'CRM customization',
+    'depends': ['crm', 'hms'],
+    'data': [
+        'views/crm_custom_views.xml',
+    ],
+    'application': True,
+    'installable': True,
+}
